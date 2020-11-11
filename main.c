8f6d74c2749f8d70914df3256520de2bdcae9402
yacine camara <camaraya254@gmail.com>
2020-11-11 09:28:42 +0000

    modularite exercice

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "exomax.h"
+int main (){
+    int x,y,c;
+do { 
+    printf ("saisir x et y:\n");
+    scanf ("%d%d", &x,&y);
+}
+while (x<0 && y<0 || x==y);
+c= max (x,y);
+ printf ("afficher %d\n", c);
+   
+    return 0;
+}
